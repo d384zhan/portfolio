c772ec36fa0992ac9c7c09c0583083762f3dc256
Dawang <180873494+d384zhan@users.noreply.github.com>
2025-10-20 19:22:36 -0400

    remove favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,13 @@ export const metadata: Metadata = {
   title: "Dawang Zhang",
   description: "Dawang Zhang's personal website",
   icons: {
-    icon: "/Garfield1978.png",
+    icon: [
+      { url: "/Garfield1978.png" },
+      { url: "/Garfield1978.png", sizes: "32x32", type: "image/png" },
+      { url: "/Garfield1978.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/Garfield1978.png",
+    shortcut: "/Garfield1978.png",
   },
 };
 
